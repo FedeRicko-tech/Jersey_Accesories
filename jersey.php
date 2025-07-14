4c0badf50afa4de9575e6584a4c6c9b55e477ca2
FedeRicko <vinsensoriko@gmail.com>
2025-07-14 10:03:18 +0700

    finish

--- jersey.php
+++ jersey.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 $pageTitle = "Jerseys - Jersey Store";
 
 $jerseys = [
@@ -156,58 +156,11 @@ sort($sizes);
 </head>
 
 <body>
-  <header class="sticky-top">
-    <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
-      <div class="container">
-        <a class="navbar-brand fw-bold fs-4" href="index.php">Legacy Sportwear</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav me-auto">
-            <li class="nav-item">
-              <a class="nav-link <?php echo ($currentPage == 'index.php') ? 'active' : ''; ?>" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link <?php echo ($currentPage == 'jersey.php') ? 'active' : ''; ?>" href="jersey.php">Jerseys</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link <?php echo ($currentPage == 'accessories.php') ? 'active' : ''; ?>" href="accessories.php">Accessories</a>
-            </li>
-          </ul>
-          <div class="d-flex align-items-center">
-            <button class="btn btn-link text-dark me-3">
-              <i class="bi bi-search"></i>
-            </button>
+<?php
+require_once 'navbar.php';
+?>
 
-<?php if (isset($_SESSION['username'])): ?>
-  <div class="dropdown me-3">
-<button class="btn btn-link text-dark dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-  <i class="bi bi-person"></i>
-</button>
 
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item" href="profil.php">Profil</a></li>
-      <li><a class="dropdown-item" href="logout.php">Logout</a></li>
-    </ul>
-  </div>
-<?php else: ?>
-  <a href="login.php" class="btn btn-link text-dark me-3">
-    <i class="bi bi-person"></i>
-  </a>
-<?php endif; ?>
-
-            <button class="btn btn-link text-dark position-relative">
-              <i class="bi bi-cart"></i>
-              <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
-                0
-              </span>
-            </button>
-          </div>
-        </div>
-      </div>
-    </nav>
-  </header>
 
   <main>
     <div class="container py-5">
